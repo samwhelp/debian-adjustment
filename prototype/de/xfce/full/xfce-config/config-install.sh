bf6c8c1b290ddd976801a768d5f3b21334b6c837
samwhelp <samwhelp@users.noreply.github.com>
2023-06-26 14:31:51 +0800

    fix

--- prototype/de/xfce/full/xfce-config/config-install.sh
+++ prototype/de/xfce/full/xfce-config/config-install.sh
@@ -10,6 +10,9 @@ set -e
 xfce_config_install () {
 
 	echo
+	echo
+	echo
+	echo
 	echo "##"
 	echo "## Config: xfce"
 	echo "##"
@@ -17,21 +20,23 @@ xfce_config_install () {
 
 	sys_xfconfd_stop
 
-	xfce_config_install_by_dir
+	xfce_config_install_overlay_home
 
-	#xfce_config_install_by_each_file
+	xfce_config_install_overlay_root
+
+	dconf_config_install
 
 
 	echo
 
 }
 
-xfce_config_install_by_dir () {
+xfce_config_install_overlay_home () {
 
 
 	echo
-	echo "mkdir -p ${HOME}/.config"
-	mkdir -p "${HOME}/.config"
+	echo "mkdir -p ${HOME}"
+	mkdir -p "${HOME}"
 
 
 	echo
@@ -42,34 +47,17 @@ xfce_config_install_by_dir () {
 
 }
 
-xfce_config_install_by_each_file () {
+
+xfce_config_install_overlay_root () {
 
 
 	echo
-	echo "mkdir -p ${HOME}/.config/xfce4/panel"
-	mkdir -p "${HOME}/.config/xfce4/panel"
-
-	echo
-	echo "install -Dm644 ./asset/overlay/etc/skel/.config/panel/whiskermenu-1.rc ${HOME}/.config/xfce4/panel/whiskermenu-1.rc"
-	install -Dm644 "./asset/overlay/etc/skel/.config/panel/whiskermenu-1.rc" "${HOME}/.config/xfce4/panel/whiskermenu-1.rc"
-
-
-	echo
-
-
-	echo
-	echo "mkdir -p ${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml"
-	mkdir -p "${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml"
-
-	echo
-	echo "install -Dm644 ./asset/overlay/etc/skel/.config/xfconf/xfce-perchannel-xml/keyboards.xml ${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml"
-	install -Dm644 "./asset/overlay/etc/skel/.config/xfconf/xfce-perchannel-xml/keyboards.xml" "${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml"
-
-
-
+	echo "sudo cp -rf ./asset/overlay/. /"
+	sudo cp -rf "./asset/overlay/." "/"
 
 
 }
+
 
 ##
 ### Tail: xfce
@@ -92,6 +80,82 @@ sys_xfconfd_stop () {
 
 ##
 ### Tail: xfconfd
+################################################################################
+
+
+################################################################################
+### Head: dconf
+##
+
+dconf_config_install () {
+
+	echo
+	echo
+	echo
+	echo
+	echo "##"
+	echo "## Config: dconf"
+	echo "##"
+	echo
+
+	dconf_config_install_prepare_db_dir
+	dconf_config_install_profile
+	dconf_config_install_update_db
+
+	echo
+
+}
+
+dconf_config_install_prepare_db_dir () {
+
+	echo
+	echo "sudo mkdir -p /etc/dconf/db/local.d"
+	sudo mkdir -p "/etc/dconf/db/local.d"
+
+	echo
+	echo "sudo mkdir -p /etc/dconf/db/site.d"
+	sudo mkdir -p "/etc/dconf/db/site.d"
+
+	echo
+	echo "sudo mkdir -p /etc/dconf/db/distro.d"
+	sudo mkdir -p "/etc/dconf/db/distro.d"
+
+}
+
+dconf_config_install_profile () {
+
+	echo
+	echo "sudo mkdir -p /etc/dconf/profile"
+	sudo mkdir -p "/etc/dconf/profile"
+
+	echo
+	echo "sudo install -Dm644 ./asset/overlay/etc/dconf/profile/user /etc/dconf/profile/user"
+	sudo install -Dm644 "./asset/overlay/etc/dconf/profile/user" "/etc/dconf/profile/user"
+
+}
+
+dconf_config_install_update_db () {
+
+	echo
+	echo "sudo dconf update"
+	sudo dconf update
+
+	echo
+	echo "file /etc/dconf/db/local"
+	file "/etc/dconf/db/local"
+
+	echo
+	echo "file /etc/dconf/db/site"
+	file "/etc/dconf/db/site"
+
+	echo
+	echo "file /etc/dconf/db/distro"
+	file "/etc/dconf/db/distro"
+
+}
+
+##
+### Tail: dconf
 ################################################################################
 
 
