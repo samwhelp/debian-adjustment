cbfac3d68c0e16f950bae602cc97db636e6a3801
samwhelp <samwhelp@users.noreply.github.com>
2024-01-26 22:19:08 +0800

    fix

---
+++ sample/develop/skel/mousepad/helper/ext/setting/module/mousepad.sh
@@ -0,0 +1,76 @@
+
+
+################################################################################
+### Head: Setting / Module / Mousepad / Variable
+##
+
+mousepad_var_init () {
+
+	return 0
+
+}
+
+mousepad_var_dump () {
+
+
+	is_not_debug && return 0
+
+
+
+
+	util_debug_echo
+	util_debug_echo "################################################################################"
+	util_debug_echo "### Head: mousepad_var_dump"
+	util_debug_echo "##"
+	util_debug_echo
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "### Tail: mousepad_var_dump"
+	util_debug_echo "################################################################################"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo
+	util_debug_echo
+
+
+	return 0
+
+}
+
+mousepad_var_dump
+
+##
+### Tail: Setting / Module / Mousepad / Variable
+################################################################################
+
+
+################################################################################
+### Head: Setting / Module / Mousepad / Portal
+##
+
+mousepad_config_install () {
+
+	util_error_echo "mousepad_config_install"
+
+	return 0
+}
+
+
+mousepad_asset_install () {
+
+	#util_error_echo "mousepad_asset_install"
+
+	util_error_echo "!!! Do Nothing !!!"
+
+	return 0
+}
+
+
+##
+### Tail: Setting / Module / Mousepad / Portal
+################################################################################
