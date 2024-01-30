2ba76a86f80ffbdf501c271ee961c1f3fff2ceb0
samwhelp <samwhelp@users.noreply.github.com>
2024-01-30 22:07:31 +0800

    fix

--- prototype/tool/mate-terminal/helper/ext/setting/module/mate-terminal.sh
+++ prototype/tool/mate-terminal/helper/ext/setting/module/mate-terminal.sh
@@ -159,10 +159,100 @@ mate_terminal_config_by_gsettings () {
 	## relocatable-schemas
 	##
 
+
 	util_error_echo
 	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" visible-name "'Default'"
 	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" visible-name "Default"
 
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" default-show-menubar false
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" default-show-menubar false
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" scrollbar-position "'hidden'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" scrollbar-position "'hidden'"
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" scroll-on-keystroke true
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" scroll-on-keystroke true
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" silent-bell true
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" silent-bell true
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" cursor-blink-mode "'off'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" cursor-blink-mode 'off'
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" use-theme-colors false
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" use-theme-colors false
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" use-custom-default-size true
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" use-custom-default-size true
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" use-system-font false
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" use-system-font false
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" font "'Monospace 14'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" font "'Monospace 14'"
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" allow-bold true
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" allow-bold true
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" bold-color "'#000000000000'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" bold-color "'#000000000000'"
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" background-type "'transparent'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" background-type "'transparent'"
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" background-darkness 0.85
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" background-darkness 0.85
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" background-color "'#000000000000'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" background-color "'#000000000000'"
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" foreground-color "'#AAAAAAAAAAAA'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" foreground-color "'#AAAAAAAAAAAA'"
+
+
+	util_error_echo
+	util_error_echo gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" palette "'#000000000000:#CCCC00000000:#4E4D9A9A0605:#C4C3A0A00000:#34346564A4A3:#7575504F7B7B:#060598979A9A:#D3D3D7D6CFCF:#555457565352:#EFEF29282928:#8A89E2E23434:#FCFBE9E84F4F:#72729F9ECFCF:#ADAC7F7EA8A8:#3434E2E2E2E2:#EEEDEEEDECEB'"
+	gsettings set "org.mate.terminal.profile:/org/mate/terminal/profiles/default/" palette "'#000000000000:#CCCC00000000:#4E4D9A9A0605:#C4C3A0A00000:#34346564A4A3:#7575504F7B7B:#060598979A9A:#D3D3D7D6CFCF:#555457565352:#EFEF29282928:#8A89E2E23434:#FCFBE9E84F4F:#72729F9ECFCF:#ADAC7F7EA8A8:#3434E2E2E2E2:#EEEDEEEDECEB'"
+
+
+
+	##
+	## dump
+	##
+
+	#gsettings list-recursively "org.mate.terminal.profile:/org/mate/terminal/profiles/default/"
+
+	#dconf dump /org/mate/terminal/profiles/default/
 
 	return 0
 
