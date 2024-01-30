4046db0a39580543a82c704516b571ada571a6b8
samwhelp <samwhelp@users.noreply.github.com>
2024-01-30 20:04:38 +0800

    fix

--- prototype/tool/gnome-terminal/helper/ext/setting/module/gnome-terminal.sh
+++ prototype/tool/gnome-terminal/helper/ext/setting/module/gnome-terminal.sh
@@ -118,8 +118,89 @@ gnome_terminal_prototype_install () {
 
 gnome_terminal_settings_set_all () {
 
+	gnome_terminal_config_by_gsettings
 
 	return 0
+}
+
+gnome_terminal_config_by_gsettings () {
+
+
+	util_error_echo
+	util_error_echo 'gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false'
+	gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+
+
+	util_error_echo
+	util_error_echo
+
+	##
+	## https://wiki.gnome.org/Apps/Terminal/FAQ
+	## $ dpkg -L gnome-terminal | grep gschema
+	## /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml
+	## $ view /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml
+	##
+
+
+	local profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
+	profile=${profile:1:-1} # remove leading and trailing single quotes
+	util_error_echo "profile=${profile}"
+
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" scrollbar-policy 'never'"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" scrollbar-policy 'never'
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" use-theme-colors false"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" use-theme-colors false
+
+
+	#util_error_echo
+	#util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" foreground-color 'rgb(255,255,255)'"
+	#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" foreground-color 'rgb(255,255,255)'
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" foreground-color 'rgb(170,170,170)'"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" foreground-color 'rgb(170,170,170)'
+
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" foreground-color 'rgb(0,0,0)'"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" background-color 'rgb(0,0,0)'
+
+
+	#util_error_echo
+	#util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" use-theme-transparency false"
+	#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" use-theme-transparency false
+
+	#util_error_echo
+	#util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" use-transparent-background true"
+	#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" use-transparent-background true
+
+	#util_error_echo
+	#util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" background-transparency-percent 15"
+	#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" background-transparency-percent 15
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" visible-name 'Default'"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" visible-name 'Default'
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" use-system-font false"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" use-system-font false
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" font 'Monospace 14'"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" font 'Monospace 14'
+
+	util_error_echo
+	util_error_echo "gsettings set \"org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/\" scrollback-unlimited true"
+	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile}/" scrollback-unlimited true
+
+
+	return 0
+
 }
 
 ##
@@ -133,13 +214,13 @@ gnome_terminal_settings_set_all () {
 
 gnome_terminal_dconfdb_put_all () {
 
-	gnome_terminal_dconfdb_put_keybind_custom
+	gnome_terminal_dconfdb_put_main
 
 	return 0
 }
 
 
-gnome_terminal_dconfdb_put_keybind_custom () {
+gnome_terminal_dconfdb_put_main () {
 
 	local source_dconfdb_file_path="${THE_GNOME_TERMINAL_SOURCE_DCONF_DB_FILE_PATH}"
 
