40e64732965310c967e7974c33858d0f544b0154
samwhelp <samwhelp@users.noreply.github.com>
2024-01-30 16:02:08 +0800

    fix

--- prototype/tool/mousepad/helper/ext/base.sh
+++ prototype/tool/mousepad/helper/ext/base.sh
@@ -214,10 +214,15 @@ base_var_init () {
 	##
 
 
-	THE_SOURCE_GSCHEMA_DIR_PATH="${THE_MASTER_ASSET_OVERLAY_DIR_PATH}/usr/share/glib-2.0/schemas"
+	THE_SOURCE_OVERLAY_DIR_PATH="${THE_MASTER_ASSET_OVERLAY_DIR_PATH}"
+	THE_TARGET_OVERLAY_DIR_PATH="/"
+
+
+	THE_SOURCE_GSCHEMA_DIR_PATH="${THE_SOURCE_OVERLAY_DIR_PATH}/usr/share/glib-2.0/schemas"
 	THE_TARGET_GSCHEMA_DIR_PATH="/usr/share/glib-2.0/schemas"
 
-	THE_SOURCE_DCONFDB_DIR_PATH="${THE_MASTER_ASSET_OVERLAY_DIR_PATH}/etc/dconf/db/distro.d"
+
+	THE_SOURCE_DCONFDB_DIR_PATH="${THE_SOURCE_OVERLAY_DIR_PATH}/etc/dconf/db/distro.d"
 	THE_TARGET_DCONFDB_DIR_PATH="/etc/dconf/db/distro.d"
 
 
@@ -376,6 +381,31 @@ base_var_dump () {
 
 
 
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path / Ext / Setting"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "THE_MAIN_EXT_SETTING_DIR_NAME=${THE_MAIN_EXT_SETTING_DIR_NAME}"
+	util_debug_echo "THE_MAIN_EXT_SETTING_DIR_PATH=${THE_MAIN_EXT_SETTING_DIR_PATH}"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path / Ext / Setting / Sub"
+	util_debug_echo "##"
+	util_debug_echo
+
+
+	util_debug_echo "THE_MAIN_EXT_SETTING_MODULE_DIR_NAME=${THE_MAIN_EXT_SETTING_MODULE_DIR_NAME}"
+	util_debug_echo "THE_MAIN_EXT_SETTING_MODULE_DIR_PATH=${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}"
+	util_debug_echo
+
+
+
+
 	##
 	############################################################################
 	##
@@ -414,6 +444,23 @@ base_var_dump () {
 	util_debug_echo
 
 
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Master Path / Model"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "THE_SOURCE_OVERLAY_DIR_PATH=${THE_SOURCE_OVERLAY_DIR_PATH}"
+	util_debug_echo "THE_TARGET_OVERLAY_DIR_PATH=${THE_TARGET_OVERLAY_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "THE_SOURCE_GSCHEMA_DIR_PATH=${THE_SOURCE_GSCHEMA_DIR_PATH}"
+	util_debug_echo "THE_TARGET_GSCHEMA_DIR_PATH=${THE_TARGET_GSCHEMA_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "THE_SOURCE_DCONFDB_DIR_PATH=${THE_SOURCE_DCONFDB_DIR_PATH}"
+	util_debug_echo "THE_TARGET_DCONFDB_DIR_PATH=${THE_TARGET_DCONFDB_DIR_PATH}"
+	util_debug_echo
 
 
 	util_debug_echo
